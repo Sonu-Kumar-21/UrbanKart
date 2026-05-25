c3962667f5d23397d73c106110d9ff54dc2f479c
Sonu-Kumar-21 <jhasonu9876@gmail.com>
2026-05-25 20:53:37 +0530

    Use HashRouter to fix static routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from './context/CartContext.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import Layout from "./components/Layout.jsx";
